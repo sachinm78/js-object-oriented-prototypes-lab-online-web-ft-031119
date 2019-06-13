e54d883ea4348261f489666cc0b54e84a33291be
Sachin Mahajan <sachinm78@gmail.com>
2019-06-13 22:25:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ function BoardMember(name, homeState, training){
   this.training = training
 }
 
-  BoardMember.prototype.veto = function(){
-    return 'No, I must disagree'
-  }
-  this.approve = function(){
-    return 'You can do that!'
-  }
-  this.doCharity = function(){
-    return "I like to help people."
-  }
+BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
+}
+this.approve = function(){
+  return 'You can do that!'
+}
+this.doCharity = function(){
+  return "I like to help people."
+}
 
-  this.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
+this.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+}
 
-  this.sayHi = function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
+this.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
