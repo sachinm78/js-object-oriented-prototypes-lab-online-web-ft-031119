2ffeb3d064d567074a58aa7a7dce54d18e314417
Sachin Mahajan <sachinm78@gmail.com>
2019-06-13 22:25:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training){
   this.training = training
 }
 
-  this.veto = function(){
+  BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
   }
   this.approve = function(){
